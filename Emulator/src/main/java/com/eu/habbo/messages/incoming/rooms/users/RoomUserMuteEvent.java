827b130ccc77933ecd5c33d6e652e04c3fab31c5
simoleo89 <simoleo89@users.noreply.github.com>
2026-06-15 22:15:39 +0200

    fix(rooms): guard room user moderation packets

--- Emulator/src/main/java/com/eu/habbo/messages/incoming/rooms/users/RoomUserMuteEvent.java
+++ Emulator/src/main/java/com/eu/habbo/messages/incoming/rooms/users/RoomUserMuteEvent.java
@@ -9,6 +9,9 @@ import com.eu.habbo.messages.incoming.MessageHandler;
 import com.eu.habbo.messages.outgoing.users.MutedWhisperComposer;
 
 public class RoomUserMuteEvent extends MessageHandler {
+    private static final int MIN_MUTE_MINUTES = 1;
+    private static final int MAX_MUTE_MINUTES = 1440;
+
     @Override
     public void handle() throws Exception {
         int userId = this.packet.readInt();
@@ -24,6 +27,12 @@ public class RoomUserMuteEvent extends MessageHandler {
             Habbo habbo = room.getHabbo(userId);
 
             if (habbo != null) {
+                if (minutes < MIN_MUTE_MINUTES || minutes > MAX_MUTE_MINUTES)
+                    return;
+
+                if (habbo.hasPermission(Permission.ACC_UNKICKABLE))
+                    return;
+
                 room.muteHabbo(habbo, minutes);
                 habbo.getClient().sendResponse(new MutedWhisperComposer(minutes * 60));
                 AchievementManager.progressAchievement(this.client.getHabbo(), Emulator.getGameEnvironment().getAchievementManager().getAchievement("SelfModMuteSeen"));

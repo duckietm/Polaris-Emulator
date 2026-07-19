6789bbec086b9f2ce106cbd4ecdad3b686565670
Dennis Object <dennisobject@gmail.com>
2026-07-19 19:45:40 +0200

    test(core): characterize rejected task policy

---
+++ Emulator/src/test/java/com/eu/habbo/threading/HabboExecutorServiceRejectionTest.java
@@ -0,0 +1,22 @@
+package com.eu.habbo.threading;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class HabboExecutorServiceRejectionTest {
+
+    @Test
+    void rejectedTaskRemainsExplicitToTheCaller() {
+        HabboExecutorService executor = new HabboExecutorService(
+                1,
+                Executors.defaultThreadFactory());
+        executor.shutdownNow();
+
+        assertThrows(RejectedExecutionException.class, () -> executor.execute(() -> {
+        }));
+    }
+}
